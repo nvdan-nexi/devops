9b30f778dbb46cf65f65ff60c0673ecb9d3f490a
Nishant Vilas Dande <nvdan@nets.eu>
2022-12-07 17:18:13 +0530

    Add rsa key for azure

--- environment.sh
+++ environment.sh
@@ -5,5 +5,5 @@ export BUILD_FROM=local
 export LOCAL_PATH=hello-world
 export REMOTE_URL=git@github.com:nvdan-nexi/hello-world.git
 export BRANCH=develop
-export id_rsa=ssh/test_github
-export id_rsa_pub=ssh/test_github.pub
+export id_rsa=ssh/id_rsa
+export id_rsa_pub=ssh/id_rsa.pub
